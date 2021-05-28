77dbc7be8da207fc2a60e8b6c896133f2b3b252e
lzhedimitri <84091518+lzhedimitri@users.noreply.github.com>
2021-05-28 12:30:52 +0300

    New homework file

---
+++ homework10.js
@@ -0,0 +1,61 @@
+var quotient = 66 / 33;
+
+var myVar = 87;
+// Only change code below this line
+myVar = myVar++;
+
+var myVar = 11;
+// Only change code below this line
+myVar = myVar--;
+
+var ourDecimal = 5.7;
+// Only change code below this line
+var myDecimal = 5.7;
+
+var product = 2.0 * 2.5;
+
+var quotient = 4.4 / 2.0; // Change this line
+
+// Only change code below this line
+var remainder = 11 % 3;
+
+var a = 3;
+var b = 17;
+var c = 12;
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+var a = 11;
+var b = 9;
+var c = 3;
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+
+var a = 5;
+var b = 12;
+var c = 4.6;
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+
+var a = 48;
+var b = 108;
+var c = 33;
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+var myFirstName = 'Dimitri';
+var myLastName = 'Pryzenko';
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+var myStr = 'FirstLine\n\t\\SecondLine\nThirdLine'; // Change this line
